76c0eac995a55fbf1d0c8dfb8213318df37a3fe3
Carter <speers.carter@gmail.com>
2025-02-04 23:08:05 -0500

    Remove Unnessecary Code from Packer.java

--- src/main/java/com/carter/speers/func/Packager.java
+++ src/main/java/com/carter/speers/func/Packager.java
@@ -4,9 +4,6 @@ import com.carter.speers.parse.model.ProjectFileModel;
 
 import java.io.IOException;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class Packager implements JavacExecutor {
     @Override
@@ -19,24 +16,22 @@ public class Packager implements JavacExecutor {
             System.err.println("Failed to package jar file. Make sure you have built your project first.");
             System.exit(1);
         }
-        List<String> classFiles = Stream.of(files)
-                .map(File::getName)
-                .filter(name -> name.endsWith(".class"))
-                .toList();
-
+  
         String jarName = String.format("%s/lib/%s.jar", pfm.getBuild().outDir(), pfm.getaPackage().jarName());
         String mainClass = pfm.getProject().mainClass();
-
-        ArrayList<String> command = new ArrayList<>(List.of("jar",
+        
+        try {
+            Process p = processBuilder
+                    .command(
+                "jar",
                 "--create",
                 "--file",
                 jarName,
                 "--main-class",
-                mainClass, "-C", pfm.getBuild().outDir(), "."));
-        try {
-            Process p = processBuilder
-                    .command(command.toArray(new String[0]))
-                    .inheritIO().start();
+                mainClass, "-C",
+                pfm.getBuild().outDir(),
+                "."
+            ).inheritIO().start();
 
             p.waitFor();
             // ProcessBuilder pb = new ProcessBuilder();
--- src/main/java/com/carter/speers/parse/ProjectTomlParser.java
+++ src/main/java/com/carter/speers/parse/ProjectTomlParser.java
@@ -1,16 +1,12 @@
 package com.carter.speers.parse;
 
-import com.carter.speers.parse.model.Project;
 import com.carter.speers.parse.model.ProjectFileModel;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Path;
 
 final public class ProjectTomlParser {
-
 
     public ProjectFileModel parseBranch(URL file) {
         TomlMapper mapper = new TomlMapper();
